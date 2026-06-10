738272d965d945dd7e8e300394762452b103cffa
Eric <14775369722@163.com>
2026-06-10 12:32:27 +0800

    Add minimal local retrieval baseline

---
+++ src/second_brain_local/__init__.py
@@ -0,0 +1,5 @@
+"""Small local retrieval helpers for Second-Brain-Local."""
+
+from .retrieval import Chunk, answer_query, load_markdown, retrieve
+
+__all__ = ["Chunk", "answer_query", "load_markdown", "retrieve"]
