1efbf59e83aa44cfdd3d545f14a5bfe7cc623246
Eric <14775369722@163.com>
2026-06-10 12:56:16 +0800

    Expose optional Ollama adapter helpers

--- src/second_brain_local/__init__.py
+++ src/second_brain_local/__init__.py
@@ -1,5 +1,14 @@
 """Small local retrieval helpers for Second-Brain-Local."""
 
+from .ollama_client import OllamaResult, build_prompt, generate_with_ollama
 from .retrieval import Chunk, answer_query, load_markdown, retrieve
 
-__all__ = ["Chunk", "answer_query", "load_markdown", "retrieve"]
+__all__ = [
+    "Chunk",
+    "OllamaResult",
+    "answer_query",
+    "build_prompt",
+    "generate_with_ollama",
+    "load_markdown",
+    "retrieve",
+]
